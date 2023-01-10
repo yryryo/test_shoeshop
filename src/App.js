55a9a1a07694c5020ade184dd3d4cd5b68e1a54a
김이현 <yryryo@gmail.com>
2023-01-10 16:57:43 +0900

    상세페이지 업로드 _사진 정렬 미적용

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
             </>
           }
         />
-        <Route path="/detail" element={<Detail/>} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
         <Route path="/about" element={<About/>} >
           <Route path="member" element={<div>멤버임</div>}></Route>
           <Route path="location" element={<About/>}></Route>
--- src/components/detail.js
+++ src/components/detail.js
@@ -1,19 +1,23 @@
 import React from "react";
+import {useParams} from "react-router-dom";
 
-function Detail(){
+function Detail(props){
+  let {id} = useParams(); //유저가 URL에 입력한 params
+  let f_id = props.shoes.find((x)=>x.id == id);
+
     return (
         <div className="container">
           <div className="row">
             <div className="col-md-6">
               <img
-                src="https://codingapple1.github.io/shop/shoes1.jpg"
+                src={"https://codingapple1.github.io/shop/shoes"+f_id+".jpg"}
                 width="100%"
               />
             </div>
             <div className="col-md-6">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
+              <h4 className="pt-5">{f_id.title}</h4>
+              <p>{f_id.content}</p>
+              <p>{f_id.price}</p>
               <button className="btn btn-danger">주문하기</button>
             </div>
           </div>
